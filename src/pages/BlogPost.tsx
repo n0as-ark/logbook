4d0c85923aaab84901c4edf722efb05a8044c4d2
Noas-Ark <more.noa.iya.25@gmail.com>
2026-05-30 19:27:21 -0400

    Update BlogPost.tsx

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -36,7 +36,7 @@ const BlogPost = () => {
       if (codeLines.length > 0) {
         elements.push(
           <pre key={key++}>
-            <code>{codeLines.map(line => line.trimStart()).join("\n")}</code>
+            <code>{codeLines.map(line => line.replace(/^\s+/, '')).join("\n")}</code>
           </pre>
         );
         codeLines = [];
