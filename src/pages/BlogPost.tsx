e96396bcadecfa10df16ed11b0f21750642d6760
Noas-Ark <more.noa.iya.25@gmail.com>
2026-06-08 02:08:04 -0400

    Update BlogPost.tsx

--- src/pages/BlogPost.tsx
+++ src/pages/BlogPost.tsx
@@ -1,6 +1,10 @@
 import { useParams, Link } from "react-router-dom";
 import { posts } from "@/data/posts";
 import { ArrowLeft } from "lucide-react";
+import { useEffect } from "react";
+import Prism from "prismjs";
+import "prismjs/components/prism-python";
+import "prismjs/themes/prism-tomorrow.css";
 
 const BlogPost = () => {
   const { slug } = useParams();
